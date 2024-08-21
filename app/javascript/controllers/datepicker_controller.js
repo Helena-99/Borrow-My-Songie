fc9a9d95a58d928b64e08e2cc96825edb2732d85
Oliver Petrus <petrus.oliver@gmail.com>
2024-08-21 14:32:13 +0100

    Homepage and Show changes

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -21,3 +21,25 @@
 //     });
 //   }
 // }
+
+
+import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
+// // Connects to data-controller="datepicker"
+export default class extends Controller {
+//   static targets = ["dates"];
+  connect() {
+    flatpickr(this.element);
+  }
+//   initFlatpickr() {
+//     const datesJson = this.datesTarget.getAttribute("data-dates-json");
+//     const datesArray = JSON.parse(datesJson);
+//     const disabledDates = datesArray.map(range => ({
+//       from: range[0],
+//       to: range[1]
+//     }));
+//     flatpickr(this.element, {
+//         disable: disabledDates,
+//     });
+//   }
+}
