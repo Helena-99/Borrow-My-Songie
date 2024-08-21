38d4e8d63690592c7dcdb5a78b67b262731e465e
Rebecca ONeill <rebecca.e.oneill3@gmail.com>
2024-08-21 14:39:05 +0100

    controller updated

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,8 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
-// Connects to data-controller="datepicker"
+// // Connects to data-controller="datepicker"
 export default class extends Controller {
+//   static targets = ["dates"];
+
   connect() {
-    flatpickr(this.element)
+    flatpickr(this.element);
   }
+
+//   initFlatpickr() {
+//     const datesJson = this.datesTarget.getAttribute("data-dates-json");
+//     const datesArray = JSON.parse(datesJson);
+//     const disabledDates = datesArray.map(range => ({
+//       from: range[0],
+//       to: range[1]
+//     }));
+
+//     flatpickr(this.element, {
+//         disable: disabledDates,
+//     });
+//   }
 }
