430095aeec27cef2eaf7b90de4424cbf4b863e27
Oliver Petrus <petrus.oliver@gmail.com>
2024-08-23 14:43:00 +0100

    Dash Sorting and Song Card

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,15 +1,16 @@
 class DashboardController < ApplicationController
   def show
-    # MY SONGS
-    # Songs owned by me [done]
     @user = current_user
     @my_songs = Song.where(user_id: @user.id)
-    @my_song = Song.last.user.id
-    # Incoming bookings on my songs [done in view]
-    # MY PURCHASES
-    @my_purchases = Booking.includes(:song).where(user_id: @user.id)
-
+    @my_purchases = Booking.includes(:song).where(user_id: @user)
+    @my_songs.each do |song|
+      song.bookings.each do |booking|
+        if Date.today >= booking.start_date && Date.today <= booking.end_date && booking.status == "accepted"
+          song.available = false
+        else
+          song.available = true
+        end
+      end
+    end
   end
 end
-
-# @my_purchases = Song.joins(:bookings).where(bookings: { user_id: @user.id })
